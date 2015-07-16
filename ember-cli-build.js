329154038332d433758a04ffbc6aad2726e17678
mzaghloul <m.salahz86@gmail.com>
2015-07-16 02:43:23 +0200

    Create Ember-CLI APP and install common used components and addons

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,5 +19,56 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  if (app.env === 'development') {
+    // bootstrap css
+    app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+    app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {destDir: 'assets'});
+    // bootstrap fonts
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {destDir: 'fonts'});
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {destDir: 'fonts'});
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {destDir: 'fonts'});
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {destDir: 'fonts'});
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {destDir: 'fonts'});
+    // add bootstrap js
+    app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+    // underscore js
+    app.import('bower_components/underscore/underscore.js');
+    // font-awesome css
+    app.import('bower_components/font-awesome/css/font-awesome.css');
+    app.import('bower_components/font-awesome/css/font-awesome.css.map', {destDir: 'assets'});
+    // font-awesome fonts
+    app.import('bower_components/font-awesome/fonts/FontAwesome.otf', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff2', {destDir: 'fonts'});
+
+  } else {
+    // bootstrap css
+    app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+    app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {destDir: 'assets'});
+    // bootstrap fonts
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {destDir: 'fonts'});
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {destDir: 'fonts'});
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {destDir: 'fonts'});
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {destDir: 'fonts'});
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {destDir: 'fonts'});
+    // add bootstrap js
+    app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+    // underscore js
+    app.import('bower_components/underscore/underscore-min.js');
+    // font-awesome css
+    app.import('bower_components/font-awesome/css/font-awesome.css');
+    app.import('bower_components/font-awesome/css/font-awesome.css.map', {destDir: 'assets'});
+    // font-awesome fonts
+    app.import('bower_components/font-awesome/fonts/FontAwesome.otf', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', {destDir: 'fonts'});
+    app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff2', {destDir: 'fonts'});
+  }
+
   return app.toTree();
 };
